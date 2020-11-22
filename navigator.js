1f7391c0fcf1846829a6da55a43ba2fc60327f8f
erickrex <59777190+erickrex@users.noreply.github.com>
2020-11-22 23:58:34 +0100

    reference
    find element inside another

---
+++ navigator.js
@@ -0,0 +1,7 @@
+/*  
+Check if the reference to an element is inside another one
+*/
+const modal = document.querySelector('.modal');
+modal.contains(button);
+modal.querySelector('button');
+!!modal.querySelector('button');
