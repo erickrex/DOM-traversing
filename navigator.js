5805d9ae671af6051a178c9efcbf9d2b6e92e73f
erickrex <59777190+erickrex@users.noreply.github.com>
2020-12-06 23:53:07 +0100

    Corrections

--- navigator.js
+++ navigator.js
@@ -104,42 +104,6 @@ modal.contains(button); //true
 modal.querySelector('button'); // button
 !!modal.querySelector('button'); //true
 
-/*Entry into a form*/
-const entryForm = document.getElementById('entryForm');
-const entriesSection = document.querySelector('#entries');
-const entryTextbox = document.querySelector('.entry-textbox');
-const entriesNav = document.querySelector('.entries-nav');
-
-let count = 1;
-function addEntryToDom(event) {
-    event.preventDefault();
-    const entryDiv = document.createElement('div');
-    entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox.value;
-    entryDiv.style.display = 'none';
-    entriesSection.appendChild(entryDiv);
-    entryTextbox.value = '';
-    
-    const displayEntryButton = document.createElement('button');
-    displayEntryButton.className = 'display-entry-button';
-    displayEntryButton.innerText = count;
-    entriesNav.appendChild(displayEntryButton);
-
-    displayEntryButton.addEventListener('click', function() {
-        const allEntries = document.querySelectorAll('.single-entry');
-        for (let index = 0; index < allEntries.length; index++) {
-            allEntries[index].style.display = 'none';
-        }
-        entryDiv.style.display = 'block';        
-    })
-    
-    count++
-}
-
-entryForm.addEventListener('submit', addEntryToDom)
-
-
-
 
 window.$ = document.querySelector.bind(document);
 window.$$ = document.querySelectorAll.bind(document);
